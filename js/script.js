799ca1db29d48df9c2acb4bd8739305bbdc2f50e
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-11 16:26:32 +0200

    preparazione pagina

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+// per inizializzare i plugin la sintassi è:
+//dayjs.extend(window.dayjs_plugin_NOME_DEL_PLUGIN);
+
+dayjs.extend(window.dayjs_plugin_customParseFormat);
+dayjs.extend(window.dayjs_plugin_relativeTime);
+
+
+dayjs.locale('it');
+
+
+const app = new Vue({
+
+  data: {
+
+  },
+
+  methods: {
+
+  }
+  
+
+});
