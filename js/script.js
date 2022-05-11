d322eea43bf2e46435fd706054ce2dfc797b9e01
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-12 01:06:25 +0200

    generare-10-email-(da-debuggare)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 
+/* 
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+
+
+
 // per inizializzare i plugin la sintassi è:
 //dayjs.extend(window.dayjs_plugin_NOME_DEL_PLUGIN);
 
@@ -11,12 +22,38 @@ dayjs.locale('it');
 
 const app = new Vue({
 
-  data: {
+  el: '#app',
 
+  data: {
+    emailList: [],
+    numberEmail: 10,
+    loadingPage: true
+    
   },
 
   methods: {
+    emailGenerator() {
+      for (let i = 0; i < this.numberEmail; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res)=>{
+          // this.generateEmail.push(res.data.response);
+          this.emailList.push(res.data.response);
+          console.log(this.emailList);
+          this.loadingPage = false;
 
+  
+        })
+
+        
+        
+        
+      }
+    }
+  },
+
+
+  mounted(){
+      this.emailGenerator()
   }
   
 
